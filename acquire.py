63769fc236d9bb38dd3d8d86b8a89e3703d52ed9
lopezelihezer <lopezelihezer@gmail.com>
2021-08-28 15:13:10 -0500

    added get telco data function

--- acquire.py
+++ acquire.py
@@ -94,6 +94,49 @@ def get_iris_data():
         
     return df
 
+
+###################### Acquire Telco Data ######################
+
+
+def new_telco_data():
+    '''
+    This function reads the telco data from the Codeup db into a df.
+    '''
+    sql_query = """
+                SELECT *
+                FROM customers
+                JOIN contract_types using(contract_type_id)
+                JOIN internet_service_types using(internet_service_type_id)
+                JOIN payment_types using(payment_type_id);
+                """
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('telco_churn'))
+    
+    return df
+
+
+def get_telco_data():
+    '''
+    This function reads in telco data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a df.
+    '''
+    if os.path.isfile('telco_churn_df.csv'):
+        
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('telco_churn_df.csv', index_col=0)
+        
+    else:
+        
+        # Read fresh data from db into a DataFrame
+        df = new_telco_data()
+        
+        # Cache data
+        df.to_csv('telco_churn_df.csv')
+        
+    return df
+
+
 # Applies transformations to iris DataFrame
 def prep_iris(df):
     df = df.drop(columns = ['species_id'])
